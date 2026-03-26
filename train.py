1a65bcd1d3d95549254395a7bffee4dee3fe7d1c
Eduardo Arino de la Rubia <earino@gmail.com>
2026-03-26 21:06:38 +0000

    exp35: n_estimators=4000 (give early stopping more room)

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ X_train, y_train = prepare(train)
 X_tr, X_val, y_tr, y_val = train_test_split(X_train, y_train, test_size=0.15, random_state=42)
 
 model = xgb.XGBClassifier(
-    n_estimators=3000,
+    n_estimators=4000,
     max_depth=7,
     learning_rate=0.01,
     subsample=0.8,
