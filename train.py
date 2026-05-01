21c5dd3978d3477df22be48aead17df32c9c34ef
szilard <szilard@users.noreply.github.com>
2026-05-01 20:30:43 +0000

    FE: DayOfWeek as numeric too

--- train.py
+++ train.py
@@ -8,8 +8,8 @@ from sklearn.model_selection import cross_val_score, StratifiedKFold
 data_dir = Path(__file__).parent / "data-cache"
 train = pd.read_csv(f"{data_dir}/2005-slice1-100k.csv")
 
-cat_cols = ["DayOfWeek", "UniqueCarrier", "Origin", "Dest", "Dep20Min", "CarrierOrigin"]
-num_cols = ["DepTime", "Distance", "DepMinute", "DayofMonthNum", "MonthNum"]
+cat_cols = ["UniqueCarrier", "Origin", "Dest", "Dep20Min", "CarrierOrigin"]
+num_cols = ["DepTime", "Distance", "DepMinute", "DayofMonthNum", "MonthNum", "DayOfWeekNum"]
 target   = "dep_delayed_15min"
 
 
@@ -25,6 +25,7 @@ def prepare(df):
     df["CarrierOrigin"] = df["UniqueCarrier"].astype(str) + "|" + df["Origin"].astype(str)
     df["DayofMonthNum"] = df["DayofMonth"].str[2:].astype(int)
     df["MonthNum"] = df["Month"].str[2:].astype(int)
+    df["DayOfWeekNum"] = df["DayOfWeek"].str[2:].astype(int)
     X = df[num_cols + cat_cols].copy()
     for col in cat_cols:
         X[col] = pd.Categorical(
