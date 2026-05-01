e863c660e86b2bd51d1eb1159164d2e81b43a31e
szilard <szilard@users.noreply.github.com>
2026-05-01 23:32:06 +0000

    n_est 350 -> 365

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ X_train, y_train = prepare(train)
 
 
 model = xgb.XGBClassifier(
-    n_estimators=350,
+    n_estimators=365,
     max_depth=14,
     learning_rate=0.06,
     min_child_weight=2.5,
